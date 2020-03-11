682b83f6edb638c4ae85f000d0cffa9c7035004e
Michael Walter <Michael_Patrick.Walter@outlook.com>
2020-03-11 16:35:28 -0600

    Finished GET's, working now on DELETE

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,25 @@
 const express = require('express');
 
+const users = require("./userDb");
+
 const router = express.Router();
 
 router.post('/', (req, res) => {
   // do your magic!
+  users.insert(req.body)
+    .then( user => {
+      if(user){
+        res.status(201).json(user)
+      }
+      else {
+        res.status(400).json({error: "Missing user data."})
+      }
+      
+    })
+    .catch(err => {
+      res.status(500).json({error: "The post could not be saved."})
+    })
+
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -12,22 +28,65 @@ router.post('/:id/posts', (req, res) => {
 
 router.get('/', (req, res) => {
   // do your magic!
+  users.get()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      rres.status(500).json({ error: "The posts information could not be retrieved." })
+    })
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  const {id} = req.params;
+  users.getById(id)
+  .then(user => {
+    res.status(200).json(user);
+  })
+  .catch(err => {
+    rres.status(500).json({ error: "The posts information could not be retrieved." })
+  })
 });
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
+  const {id} = req.params;
+  users.getUserPosts(id)
+    .then(user => {
+      res.status(200).json({user});
+    })
+    .catch(err => {
+      rres.status(500).json({ error: "The posts information could not be retrieved." })
+    })
+  
 });
 
 router.delete('/:id', (req, res) => {
   // do your magic!
+  const {id} = req.params.id;
+
+  users.remove(id)
+  .then(user => {
+
+      res.status(200).json(user, {success: "Delete Success."})
+    
+  })
+  .catch(err => {
+    res.status(500).json({ error: "The posts information could not be retrieved." })
+  })
+
 });
 
 router.put('/:id', (req, res) => {
   // do your magic!
+  const {id} = req.params.id;
+  const changes = req.body;
+
+  users.update(id, changes)
+  if(!changes.name){
+    res.status(400).json({errorMessage: "Missing name"})
+  }
 });
 
 //custom middleware
